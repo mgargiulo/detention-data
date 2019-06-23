0e8a7dcf16a198eb022590da0ebd1833c7f39d74
floranwang <fwang7@stanford.edu>
2019-06-23 15:08:32 -0400

    Add files via upload
    fixed commas in numbers

--- example1.R
+++ example1.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
 # load datasets taken from Department of Homeland Security
-inadmissible_aliens = read.csv("yrbk_2017_enf_excel_final/fy2017_table38.csv")
-granted_asylum = read.csv("YRBK 2017 RFA Excel Final/fy2017_table16.csv")
+inadmissible_aliens = read.csv("yrbk_2017_enf_excel_final/fy2017_table38.csv", stringsAsFactors = FALSE)
+granted_asylum = read.csv("YRBK 2017 RFA Excel Final/fy2017_table16.csv", stringsAsFactors = FALSE)
 
 # preprocessing
 colnames(inadmissible_aliens)[colnames(inadmissible_aliens)=="Total"] <- "Total_inadmissible"
@@ -10,7 +10,9 @@ colnames(granted_asylum)[colnames(granted_asylum)=="Total"] <- "Total_asylees"
 
 combined = merge(inadmissible_aliens, granted_asylum, by.x = "Year")
 combined$Year = as.factor(combined$Year)
+combined$Total_asylees = gsub(",", "", combined$Total_asylees)
 combined$Total_asylees = as.numeric(combined$Total_asylees)
+combined$Total_inadmissible = gsub(",", "", combined$Total_inadmissible)
 combined$Total_inadmissible = as.numeric(combined$Total_inadmissible)
 summary(combined$Total_asylees)
 
